f7794ec96dc25f5c36ecc301ad8e1dbd6647eeba
Junyi BEI <beijunyi@gmail.com>
2013-07-27 20:22:37 +0100

    added download button

--- web/src/main/java/com/grucis/web/controller/BitmapGalleryController.java
+++ web/src/main/java/com/grucis/web/controller/BitmapGalleryController.java
@@ -4,7 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 import com.grucis.dev.service.OutputModelService;
@@ -42,6 +44,16 @@ public final class BitmapGalleryController {
   public byte[] getImage(@PathParam("id") int id) throws IOException {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     ImageIO.write(outputModelService.getImage(id).getImage(), "png", out);
+    return out.toByteArray();
+  }
+
+  @GET
+  @Path("/download/{id}.png")
+  @Produces("image/png")
+  public byte[] downloadImage(@PathParam("id") int id, @Context HttpServletResponse response) throws IOException {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ImageIO.write(outputModelService.getImage(id).getImage(), "png", out);
+    response.addHeader("Content-Disposition", "attachment; filename=\"" + id + ".png\"");
     return out.toByteArray();
   }
 }
--- web/src/main/webapp/gde/view/OffsetImageCanvas.js
+++ web/src/main/webapp/gde/view/OffsetImageCanvas.js
@@ -6,6 +6,20 @@ Ext.define('GDE.view.OffsetImageCanvas', {
   initComponent: function () {
     var me = this;
     Ext.apply(me, {
+      tbar: [
+        {
+          icon: 'resources/images/disk_blue.png',
+          text: 'Download',
+          id: 'download',
+          disabled: true,
+          handler: function() {
+            Ext.Array.each(me.bitmaps, function(bitmap) {
+              window.location = 'api/bitmap/download/' + bitmap.name;
+            });
+          }
+        }
+      ],
+
       drawPlan: [
         function () {
           Ext.ux.EaselPanelUtils.drawScales(me.stage, {
@@ -29,23 +43,27 @@ Ext.define('GDE.view.OffsetImageCanvas', {
         me.stage.update();
       },
 
+      unload: function() {
+        Ext.Array.each(me.bitmaps, function(bitmap) {
+          me.stage.removeChild(bitmap);
+        });
+        me.bitmaps = [];
+        me.adrns = {};
+        me.down('#download').disable();
+      },
+
       loadImage: function (adrn, keepPrevious) {
         var name = adrn.get('id') + '.png';
         var url = 'api/bitmap/image/' + name;
         var queue = new createjs.LoadQueue(false);
-        if(!keepPrevious) {
-          Ext.Array.each(me.bitmaps, function(bitmap) {
-            me.stage.removeChild(bitmap);
-          });
-          me.bitmaps = [];
-          me.adrns = {};
-        }
+        if(!keepPrevious) me.unload();
         queue.addEventListener('fileload', function(payload) {
           var bitmap = new createjs.Bitmap(payload.result);
           bitmap.name = name;
           me.bitmaps.push(bitmap);
           me.adrns[name] = adrn;
           me.placeImage(bitmap);
+          me.down('#download').enable();
         });
         queue.loadFile(url);
       }
