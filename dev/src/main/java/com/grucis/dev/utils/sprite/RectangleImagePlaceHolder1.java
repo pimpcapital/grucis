bfdcaa0f2532d32bbb519b42cc9545de391b2f04
beijunyi <beijunyi@gmail.com>
2013-08-09 14:43:09 +0100

    fix file name

--- dev/src/main/java/com/grucis/dev/utils/sprite/RectangleImagePlaceHolder1.java
+++ dev/src/main/java/com/grucis/dev/utils/sprite/RectangleImagePlaceHolder1.java
@@ -1,11 +1,11 @@
 package com.grucis.dev.utils.sprite;
 
-public final class RectangleImagePlaceholder implements Comparable<RectangleImagePlaceholder> {
+public final class RectangleImagePlaceHolder1 implements Comparable<RectangleImagePlaceHolder1> {
   private final int id;
   private final int width;
   private final int height;
 
-  public RectangleImagePlaceholder(int id, int width, int height) {
+  public RectangleImagePlaceHolder1(int id, int width, int height) {
     this.id = id;
     this.width = width;
     this.height = height;
@@ -23,7 +23,7 @@ public final class RectangleImagePlaceholder implements Comparable<RectangleImag
     return height;
   }
 
-  public int compareTo(RectangleImagePlaceholder o) {
+  public int compareTo(RectangleImagePlaceHolder1 o) {
     int c = Integer.compare(o.getWidth(), getWidth());
     if(c != 0) return c;
     return Integer.compare(o.getHeight(), getHeight());
