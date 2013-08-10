3205129976c18ddd5ad724e1ad1b36da0dbd6645
Junyi BEI <beijunyi@gmail.com>
2013-08-10 23:35:31 +0100

    index using id instead of map element id

--- dev/src/main/java/com/grucis/dev/mapper/export/TileMapMapper.java
+++ dev/src/main/java/com/grucis/dev/mapper/export/TileMapMapper.java
@@ -2,19 +2,37 @@ package com.grucis.dev.mapper.export;
 
 import com.grucis.dev.model.export.map.TileMap;
 import com.grucis.dev.model.output.SaMap;
+import com.grucis.dev.service.RawModelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public final class TileMapMapper extends ExportModelMapper<SaMap, TileMap> {
+
+  @Autowired
+  private RawModelService rawModelService;
+
   @Override
   public TileMap map(SaMap source) {
     TileMap ret = new TileMap(source.getId());
 
     ret.setName(source.getName());
-    ret.setEast(source.getEast());
-    ret.setSouth(source.getSouth());
-    ret.setTiles(source.getTiles());
-    ret.setObjects(source.getObjects());
+    int east = source.getEast();
+    ret.setEast(east);
+    int south = source.getSouth();
+    ret.setSouth(south);
+    int[][] mapElementTiles = source.getTiles();
+    int[][] mapElementObjects = source.getObjects();
+    int[][] tiles = new int [south][east];
+    int[][] objects = new int [south][east];
+    for(int s = 0; s < south; s++) {
+      for(int e = 0; e < east; e++) {
+        tiles[s][e] = rawModelService.getAdrnByMapElementId(mapElementTiles[s][e]).getId();
+        objects[s][e] = rawModelService.getAdrnByMapElementId(mapElementObjects[s][e]).getId();
+      }
+    }
+    ret.setTiles(tiles);
+    ret.setObjects(objects);
 
     return ret;
   }
