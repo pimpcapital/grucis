d08e5ffca81bd7069fe6d777626ecdb6700c32eb
beijunyi <beijunyi@gmail.com>
2013-08-23 14:57:08 +0100

    improves tile map canvas?

--- web/src/main/webapp/gde/view/TileMapCanvas.js
+++ web/src/main/webapp/gde/view/TileMapCanvas.js
@@ -1,39 +1,43 @@
 Ext.define('GDE.view.TileMapCanvas', {
   extend: 'Ext.ux.EaselPanel',
   alias: 'widget.tilemap',
-  imageCache: {},
+  cache: {},
   children: [],
-  offset: {x: 0, y: 0},
   initComponent: function () {
     var me = this;
     Ext.apply(me, {
       drawPlan: [
         function () {
-          me.setRadius();
           if(me.map) {
-            me.stage.removeAllChildren();
-            me.children = [];
-            me.offset = {x: 0, y: 0};
+            me.setReference();
             me.loadAndDraw();
           }
         }
       ],
 
-      setRadius: function () {
-        var width = me.canvasEl.width;
-        var height = me.canvasEl.height;
-        me.centerPix = {x: Math.floor(width / 2), y: Math.floor(height / 2)};
-        me.radius = Math.ceil(Math.sqrt(Math.pow(width / 2, 2) + Math.pow(height / 2, 2)) / 80);
+      setReference: function () {
+        if(!me.reference) {
+          me.reference = {
+            x: Math.floor(me.canvasEl.width / 2),
+            y: Math.floor(me.canvasEl.height / 2),
+            south: Math.floor(me.map.south / 2),
+            east: Math.floor(me.map.east / 2)
+          }
+        } else {
+          me.reference.x = Math.min(me.canvasEl.width, me.reference.x);
+          me.reference.y = Math.min(me.canvasEl.height, me.reference.y);
+        }
+        me.radius = Math.ceil(me.canvasEl.width / 64 + me.canvasEl.height / 48);
       },
 
       loadAndDraw: function () {
         var load = [];
-        for(var s = Math.max(0, me.center.south - me.radius); s <= Math.min(me.map.south - 1, me.center.south + me.radius); s++) {
-          for(var e = Math.max(0, me.center.east - me.radius); e <= Math.min(me.map.east - 1, me.center.east + me.radius); e++) {
+        for(var s = Math.max(0, me.reference.south - me.radius); s <= Math.min(me.map.south - 1, me.reference.south + me.radius); s++) {
+          for(var e = Math.max(0, me.reference.east - me.radius); e <= Math.min(me.map.east - 1, me.reference.east + me.radius); e++) {
             var tile = me.map.tiles[s][e];
             var object = me.map.objects[s][e];
-            if(tile != -1 && !me.imageCache[tile]) Ext.Array.include(load, tile);
-            if(object != -1 && !me.imageCache[object]) Ext.Array.include(load, object);
+            if(tile != -1 && !me.cache[tile]) Ext.Array.include(load, tile);
+            if(object != -1 && !me.cache[object]) Ext.Array.include(load, object);
           }
         }
         if(load.length) {
@@ -53,10 +57,10 @@ Ext.define('GDE.view.TileMapCanvas', {
           queue.addEventListener('fileload', function (payload) {
             var id = payload.item.id.split('_');
             var imageId = parseInt(id[1]);
-            var image = me.imageCache[imageId];
+            var image = me.cache[imageId];
             if(!image) {
               image = {};
-              me.imageCache[imageId] = image;
+              me.cache[imageId] = image;
             }
             image[id[0]] = payload.result;
           });
@@ -70,28 +74,34 @@ Ext.define('GDE.view.TileMapCanvas', {
 
       },
 
-      isImageVisible: function (xOffset, yOffset, index) {
-        if(xOffset > 0) {
-          if(xOffset - index.regX > me.canvasEl.width / 2 + 128) return false;
-        } else {
-          if(-xOffset - (index.width - index.regX) > me.canvasEl.width / 2 + 128) return false;
-        }
-        if(yOffset > 0) {
-          if(yOffset - index.regY > me.canvasEl.height / 2 + 96) return false;
-        } else {
-          if(-yOffset - (index.height - index.regY) > me.canvasEl.height / 2 + 96) return false;
-        }
+      isInBuffer: function (x, y, elementIndex) {
+        var width = elementIndex.width;
+        var height = elementIndex.height;
+        var regX = elementIndex.regX;
+        var regY = elementIndex.regY;
+
+        var left = x - regX;
+        if(left < -me.canvasEl.width) return false;
+        var right = left + width;
+        if(right > me.canvasEl.width * 2) return false;
+        var top = y - regY;
+        if(top < -me.canvasEl.height) return false;
+        var bottom = top + height;
+        if(bottom > me.canvasEl * 2) return false;
+
         return true;
       },
 
-      drawElement: function (south, east, element, index, z) {
-        if(!element) return;
-        var xOffset = (south + east - me.center.south - me.center.east) * 32;
-        var yOffset = (south - east - me.center.south + me.center.east) * 24;
+      drawElement: function (south, east, id, index, z) {
+        var element = me.cache[id];
+        var xOffset = (south + east - me.reference.south - me.reference.east) * 32;
+        var yOffset = (south - east - me.reference.south + me.reference.east) * 24;
 
         var child = me.children[index];
         var elementIndex = element.index;
-        if(me.isImageVisible(xOffset, yOffset, element.index)) {
+        var x = me.reference.x + xOffset;
+        var y = me.reference.y + yOffset;
+        if(me.isInBuffer(x, y, elementIndex)) {
           if(!child) {
             child = new createjs.Bitmap(element.bitmap);
             child.regX = elementIndex.regX;
@@ -99,53 +109,31 @@ Ext.define('GDE.view.TileMapCanvas', {
             child.south = south;
             child.east = east;
             child.z = z;
-            child.x = me.centerPix.x + xOffset + me.offset.x;
-            child.y = me.centerPix.y + yOffset + me.offset.y;
+            child.x = me.reference.x + xOffset;
+            child.y = me.reference.y + yOffset;
             child.update = new Date().getMilliseconds();
             me.children[index] = child;
             me.stage.addChild(child);
             child.addEventListener("mousedown", function (evt) {
-              console.log('south: ' + evt.target.south + ', east: ' + evt.target.east);
               var offset = {x: evt.target.x - evt.stageX, y: evt.target.y - evt.stageY};
+              me.reference.south = evt.target.south;
+              me.reference.east = evt.target.east;
               evt.addEventListener("mousemove", function (ev) {
                 var deltaX = ev.stageX + offset.x - ev.target.x;
                 var deltaY = ev.stageY + offset.y - ev.target.y;
                 me.offset.x += deltaX;
                 me.offset.y += deltaY;
-                Ext.Array.each(me.stage.children, function(obj) {
+                Ext.Array.each(me.stage.children, function (obj) {
                   obj.x += deltaX;
                   obj.y += deltaY;
                 });
+                me.reference.x += evt.target.x;
+                me.reference.y = evt.target.y;
                 me.stage.update();
-
-//                var load = false;
-//                while(me.offset.x > 32) {
-//                  me.center.south++;
-//                  me.center.east++;
-//                  me.offset.x -= 32;
-//                  load = true;
-//                }
-//                while(me.offset.x < -32) {
-//                  me.center.south--;
-//                  me.center.east--;
-//                  me.offset.x += 32;
-//                  load = true;
-//                }
-//                while(me.offset.y > 24) {
-//                  me.center.south--;
-//                  me.center.east++;
-//                  me.offset.y -= 24;
-//                  load = true;
-//                }
-//                while(me.offset.y < -24) {
-//                  me.center.south++;
-//                  me.center.east--;
-//                  me.offset.y += 24;
-//                  load = true;
-//                }
-//                if(load) me.loadAndDraw();
-
               });
+              evt.addEventListener("mouseup", function(evt) {
+                me.loadAndDraw();
+              })
             });
           }
         } else {
@@ -160,24 +148,23 @@ Ext.define('GDE.view.TileMapCanvas', {
         var sortFn = function (a, b) {
           if(a.z < b.z) return -1;
           if(a.z > b.z) return 1;
-          if(a.south > b.south) return 1;
-          if(a.south < b.south) return -1;
           if(a.east > b.east) return -1;
           if(a.east < b.east) return 1;
+          if(a.south > b.south) return 1;
+          if(a.south < b.south) return -1;
           return a.update - b.update;
         };
         me.stage.sortChildren(sortFn);
       },
 
       drawMap: function () {
-        for(var e = Math.min(me.map.east - 1, me.center.east + me.radius); e >= Math.max(0, me.center.east - me.radius); e--) {
-          for(var s = Math.max(0, me.center.south - me.radius); s <= Math.min(me.map.south - 1, me.center.south + me.radius); s++) {
+        for(var e = Math.min(me.map.east - 1, me.reference.east + me.radius); e >= Math.max(0, me.reference.east - me.radius); e--) {
+          for(var s = Math.max(0, me.reference.south - me.radius); s <= Math.min(me.map.south - 1, me.reference.south + me.radius); s++) {
             var index = e * me.map.south + s;
             var tileId = me.map.tiles[s][e];
+            if(tileId != -1) me.drawElement(s, e, tileId, index, 0);
             var objectId = me.map.objects[s][e];
-
-            me.drawElement(s, e, me.imageCache[tileId], index, 0);
-            me.drawElement(s, e, me.imageCache[objectId], me.total + index, 1);
+            if(objectId != -1) me.drawElement(s, e, objectId, me.total + index, 1);
           }
         }
         me.sortElements();
@@ -201,7 +188,7 @@ Ext.define('GDE.view.TileMapCanvas', {
         queue.addEventListener('fileload', function (payload) {
           me.map = payload.result;
           me.total = me.map.east * me.map.south;
-          me.center = {south: Math.floor(me.map.south / 2), east: Math.floor(me.map.east / 2)};
+          me.setReference();
           me.loadAndDraw();
         });
         queue.loadFile(url);
