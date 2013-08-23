c67db28758495f02c77de42d4a4afbbbe7632f48
beijunyi <beijunyi@gmail.com>
2013-08-23 18:12:43 +0100

    add export all in services

--- web/src/main/java/com/grucis/web/controller/AnimationGalleryController.java
+++ web/src/main/java/com/grucis/web/controller/AnimationGalleryController.java
@@ -1,9 +1,15 @@
 package com.grucis.web.controller;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
+import com.grucis.dev.io.ExportManager;
+import com.grucis.dev.model.progress.ExportProgress;
 import com.grucis.dev.service.ExportModelService;
 import com.grucis.dev.service.RawModelService;
 import com.grucis.dev.utils.image.ImageUtils;
@@ -18,12 +24,18 @@ import org.springframework.stereotype.Controller;
 @Path("api/animation")
 @Produces("application/json")
 public final class AnimationGalleryController {
+  public static final String ANIMATION_EXPORT_PROGRESS = "ANIMATION_EXPORT_PROGRESS";
+
   @Autowired
   private RawModelService rawModelService;
   @Autowired
   private SprAdrnViewMapper sprAdrnViewMapper;
   @Autowired
   private ExportModelService exportModelService;
+  @Autowired
+  private ExecutorService executorService;
+  @Autowired
+  private ExportManager exportManager;
 
   @GET
   @Path("/spradrn")
@@ -48,5 +60,28 @@ public final class AnimationGalleryController {
   @Cache(maxAge = Integer.MAX_VALUE)
   public Response getAnimationReference(@PathParam("id") int id) {
     return Response.ok(exportModelService.getAnimationSpriteIndex(id, false)).build();
+  }
+
+  @POST
+  @Path("/export/start")
+  public Response startExport(@Context HttpServletRequest request) {
+    HttpSession session = request.getSession(true);
+    if(session.getAttribute(ANIMATION_EXPORT_PROGRESS) == null) {
+      final ExportProgress progress = new ExportProgress();
+      session.setAttribute(ANIMATION_EXPORT_PROGRESS, progress);
+      executorService.submit(new Runnable() {
+        public void run() {
+          exportManager.exportAnimations(progress);
+        }
+      });
+    }
+    return Response.ok().build();
+  }
+
+  @GET
+  @Path("/export/update")
+  public Response updateExportProgress(@Context HttpServletRequest request) {
+    HttpSession session = request.getSession(true);
+    return Response.ok(session.getAttribute(ANIMATION_EXPORT_PROGRESS)).build();
   }
 }
--- web/src/main/java/com/grucis/web/controller/MapGalleryController.java
+++ web/src/main/java/com/grucis/web/controller/MapGalleryController.java
@@ -1,9 +1,15 @@
 package com.grucis.web.controller;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
+import com.grucis.dev.io.ExportManager;
+import com.grucis.dev.model.progress.ExportProgress;
 import com.grucis.dev.service.DictionaryModelService;
 import com.grucis.dev.service.ExportModelService;
 import com.grucis.web.mapper.MapViewMapper;
@@ -18,12 +24,18 @@ import org.springframework.stereotype.Controller;
 @Produces("application/json")
 public final class MapGalleryController {
 
+  public static final String MAP_EXPORT_PROGRESS = "MAP_EXPORT_PROGRESS";
+
   @Autowired
   private DictionaryModelService dictionaryModelService;
   @Autowired
   private ExportModelService exportModelService;
   @Autowired
   private MapViewMapper mapViewMapper;
+  @Autowired
+  private ExecutorService executorService;
+  @Autowired
+  private ExportManager exportManager;
 
   @GET
   @Path("/maps")
@@ -40,6 +52,29 @@ public final class MapGalleryController {
   @GZIP
   public Response getMap(@PathParam("id") int id) {
     return Response.ok(exportModelService.getTileMap(id, false)).build();
+  }
+
+  @POST
+  @Path("/export/start")
+  public Response startExport(@Context HttpServletRequest request) {
+    HttpSession session = request.getSession(true);
+    if(session.getAttribute(MAP_EXPORT_PROGRESS) == null) {
+      final ExportProgress progress = new ExportProgress();
+      session.setAttribute(MAP_EXPORT_PROGRESS, progress);
+      executorService.submit(new Runnable() {
+        public void run() {
+          exportManager.exportMaps(progress);
+        }
+      });
+    }
+    return Response.ok().build();
+  }
+
+  @GET
+  @Path("/export/update")
+  public Response updateExportProgress(@Context HttpServletRequest request) {
+    HttpSession session = request.getSession(true);
+    return Response.ok(session.getAttribute(MAP_EXPORT_PROGRESS)).build();
   }
 
 }
